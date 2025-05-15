54ddee346b44338ee15315d7f2ef0e618b970926
zhihang1028 <zhihang1028@student.usm.my>
2025-05-15 10:46:42 +0800

    Protected Keyword

--- classTesting2.cpp
+++ classTesting2.cpp
@@ -5,12 +5,19 @@ class Square{
     private:
     int length;
 
+    protected:
+    int height;
+
     public:
-    Square(int _length = 0){length = _length;}
+    Square(int _length = 0, int _height = 0){length = _length; height = _height;}
     void setLength(int i = 0){length = i;}
-    int getLength(){return length;}
-    int getArea(){return length * length;}
-    void printArea(){cout << "Area of Square: " << getArea() << endl;}
+    int getLength()const {return length;}
+
+    int getArea()const {return length * length;}
+    void printArea()const {cout << "Area of Square: " << getArea() << endl;}
+
+    int getVolume()const {return getArea() * height;}
+    void printVolume()const {cout << "Volume of Square(Cube): " << getVolume() << endl;}
 };
 
 class Rectangle : public Square{
@@ -18,11 +25,15 @@ class Rectangle : public Square{
     int width;
 
     public:
-    Rectangle(int _width = 0){width = _width;}
+    Rectangle(int _length = 0, int _width = 0, int _height = 0){setLength(_length); width = _width; height = _height;}
     void setWidth(int i = 0){width = i;}
-    int getWidth(){return width;}
-    int getArea(){return getLength() * width;}
-    void printArea(){cout << "Area of Rectangle: " << getArea() << endl;}
+    int getWidth()const {return width;}
+
+    int getArea()const {return getLength() * width;}
+    void printArea()const {cout << "Area of Rectangle: " << getArea() << endl;}
+
+    int getVolume()const {return getArea() * height;}
+    void printVolume() const {cout << "Volume of Rectangle(Cuboid): " << getVolume() << endl;}
 
 };
 
@@ -41,7 +52,18 @@ int main(){
 
     Square s2;
     s2.printArea();
-    
+
+    Square s3(100);
+    s3.printArea();
+
+    Rectangle r2(10, 5);
+    r2.printArea();
+
+    Square s4(3, 3);    // Actually no need pass height parameter bcs Cube same length all sides, but for demo purpose of proctected.
+    s4.printVolume();
+
+    Rectangle r3(3, 4, 5);
+    r3.printVolume();
 
     //cin.ignore();
     //cin.get();
